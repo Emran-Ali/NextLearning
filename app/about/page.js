1fe2e39f08f87513121115a76d2001fedaf545c9
Emran-Ali <emranitmbstu@gmail.com>
2024-03-30 02:03:45 +0600

    Initialize Project

---
+++ app/about/page.js
@@ -0,0 +1,6 @@
+
+export default function page() {
+  return (
+    <div className="fixed bottom-0 bg-cyan-500 w-full">Hello About</div>
+  )
+}
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,8 +11,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="p-2">
+      <body className={inter.className}>
+        <nav className="w-full bg-cyan-500">
+          <button className="m-2 p-2 border-2 rounded-xl">
+            <Link href="/"> Home </Link>
+          </button>
+          <Link href="/about">
+            <button className="m-2 p-2 border-2 rounded-xl">About</button>
+          </Link>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
